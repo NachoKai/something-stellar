51b7a1aab730e3f2c125bc74f5e4faf684394bbe
Nacho Caiafa <ignacio.caiafa@gmail.com>
2022-06-12 15:22:26 -0300

    move chakra provider to the index

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
-import { Box, ChakraProvider, Image } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
 
 import SaturnSVG from './assets/saturn.svg'
 import Main from './components/Main'
 
 function App() {
 	return (
-		<ChakraProvider>
-			<Box
-				alignItems="center"
-				display="flex"
-				flexDirection="column"
-				justifyContent="center"
-				p={2}
-				pt={6}
-			>
-				<Image alt="Saturn" boxSize="10vw" mb={8} objectFit="cover" src={SaturnSVG} />
-				<Main />
-			</Box>
-		</ChakraProvider>
+		<Box
+			alignItems="center"
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+			p={3}
+			pt={6}
+		>
+			<Image alt="Saturn" boxSize="10vw" mb={8} objectFit="cover" src={SaturnSVG} />
+			<Main />
+		</Box>
 	)
 }
 
